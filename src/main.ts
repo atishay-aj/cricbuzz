d3cbde3c2af067c39fcfdf4dbf24af93c728ed95
Atishay Jain <atishay.jain@awiros.com>
2025-03-18 19:51:57 +0530

    add Dockerfile for containerization and update .env.example with PORT variable

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
+// other than the main requirements i added Ehancments:
+// 1.Added Swagger documentation
+// 2.Added Dockerfile for containerization
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
